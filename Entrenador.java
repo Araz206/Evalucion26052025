3187732aeead9ee6e295f57faaae942527ab499c
Emily Lisbeth Bolaño Flores <2429841@clases.edu.sv>
2025-05-26 10:56:39 -0600

    cambios de la clases

--- Entrenador.java
+++ Entrenador.java
@@ -1,48 +1,8 @@
 public class Entrenador {
 
-    private int id;
-    private String nombre;
-    private String apellidos;
-    private int edad;
+private string IdFederacion;
+public Entrenador() {
+    // Constructor por defecto
 
-    public Entrenador(int id, String nombre, String apellidos, int edad) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.edad = edad;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellidos() {
-        return apellidos;
-    }
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    @Override
-    public String toString() {
-        return "Entrenador{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                ", edad=" + edad +
-                '}';
-    } 
+}
 }
--- futbolista.java
+++ futbolista.java
@@ -1,48 +1,9 @@
 public class futbolista {
 
-    private int id;
-    private String nombre;
-    private String apellidos;
-    private int edad;
+private int dorsal;
+private string demarcacion;
 
-    public futbolista(int id, String nombre, String apellidos, int edad) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.edad = edad;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellidos() {
-        return apellidos;
-    }
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-    public int getEdad() {
-        return edad;
-    }
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    @Override
-    public String toString() {
-        return "futbolista{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                ", edad=" + edad +
-                '}';
-    }
-
+public futbolista() {
+    // Constructor por defecto
+}
 }
